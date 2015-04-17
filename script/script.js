200595ea8c74e79a4971097c216d076572faf433
wolfgangleon <wolfgangrleonh@gmail.com>
2015-04-17 15:25:00 -0400

    contact form and reviews

--- script/script.js
+++ script/script.js
@@ -2,5 +2,17 @@ $(document).ready(function(){
 	
 	var screenHeight = $(window).height();
 
+	$('#contactLink').on('click', function() {
+		$('html, body').animate({ 
+	      scrollTop: $('#contact').offset().top 
+	  	}, 1000);
+	});
+
+	$('#infoLink').on('click', function() {
+		$('html, body').animate({ 
+	      scrollTop: $('#info').offset().top 
+	  	}, 1000);
+	})
+
 	//$('#carouselHeader .thumbnail').height( screenHeight ) //Set screen size
 });
