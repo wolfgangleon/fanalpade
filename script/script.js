e4283a6e6416b0e5c3d1065469ceb138d8b83d36
wolfgangleon <wolfgangrleonh@gmail.com>
2015-04-20 13:27:25 -0400

    reviews masonry sections done

--- script/script.js
+++ script/script.js
@@ -8,5 +8,14 @@ $(document).ready(function(){
 	  	}, 1000);
 	});
 
+	//reviews section masonry --->
+	var container = $('#reviews .container');
+	// initialize
+	container.masonry({
+	  columnWidth: 200,
+	  itemSelector: '.panel',
+	  isFitWidth: true
+	});
+
 	//$('#carouselHeader .thumbnail').height( screenHeight ) //Set screen size
 });
